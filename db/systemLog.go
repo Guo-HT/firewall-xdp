6447b4f2a13bb9913ff833b6fc5d9abbdd535a44
Guo-HT <1622761893@qq.com>
2023-03-25 00:36:35 +0800

    [REP]修复一些bug

--- db/systemLog.go
+++ db/systemLog.go
@@ -12,9 +12,11 @@ func SetSystemLog(ip string, user string, option string, optResult bool) {
 
 // SearchSystemLog 搜索系统日志
 func SearchSystemLog(search string, startTime, endTime, pageNo, pageSize int, sort string) (sysLog []SystemLog, total int) {
+	var sysLogTmp []SystemLog
 	searchLike := "%" + search + "%"
 	offset := (pageNo - 1) * pageSize
-	result := dbConn.Model(&SystemLog{}).Where("(ip like ? or option like ? or username like ?) and create_at > ? and create_at < ?", searchLike, searchLike, searchLike, startTime, endTime).Order("create_at " + sort).Limit(pageSize).Offset(offset).Find(&sysLog)
-	total = int(result.RowsAffected)
+	_ = dbConn.Model(&SystemLog{}).Where("(ip like ? or option like ? or username like ?) and create_at > ? and create_at < ?", searchLike, searchLike, searchLike, startTime, endTime).Order("create_at " + sort).Limit(pageSize).Offset(offset).Find(&sysLog)
+	resultCount := dbConn.Model(&SystemLog{}).Where("(ip like ? or option like ? or username like ?) and create_at > ? and create_at < ?", searchLike, searchLike, searchLike, startTime, endTime).Order("create_at " + sort).Find(&sysLogTmp)
+	total = int(resultCount.RowsAffected)
 	return
 }
